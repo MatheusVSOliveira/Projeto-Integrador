119f47cbfec07f79f9d1e382cb876b8ab33002ca
Raimundo Machado <larissainaciom.dacosta@gmail.com>
2021-10-04 09:02:06 -0300

    criando temaRepository

---
+++ backend/Tales/src/main/java/br/org/generation/projetointegrador/repository/TemaRepository.java
@@ -0,0 +1,10 @@
+package br.org.generation.projetointegrador.repository;
+
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import br.org.generation.projetointegrador.model.Tema;
+
+public interface TemaRepository extends JpaRepository<Tema, Long> {
+    public List <Tema> findAllByNomeContainingIgnoreCase (String descricao);
+
+}
